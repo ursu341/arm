447ec198320d90040d25785cda33a08f39b5a058
QalandarDev <uzwebdevolopersn2@gmail.com>
2022-04-19 12:15:09 +0500

    +index image

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -197,7 +197,7 @@ $this->title = 'UrDU Axborot resurs markazi';
                             ?>
                             <div class="row">
                                 <div class="col-md-3">
-                                    <img src="<?= Yii::getAlias('@web') . $item->img ?>" class="img-thumbnail"
+                                    <img src="<?= Yii::getAlias('@web') .'/images/'. $item->img ?>" class="img-thumbnail"
                                          style="width: 300px" alt="img">
                                 </div>
                                 <div class="col-md-9">
