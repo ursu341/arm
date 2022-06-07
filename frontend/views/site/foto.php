65f89af12dd7e7cccfd955c51f8dbe62e620f25c
Qalandar Dev <qalandar@urdu.uz>
2022-06-07 17:41:36 +0500

    vh

--- frontend/views/site/foto.php
+++ frontend/views/site/foto.php
@@ -10,32 +10,35 @@ use kv4nt\owlcarousel\OwlCarouselWidget;
 use yii\bootstrap\Html;
 
 ?>
-<div>
-    <?php
-    OwlCarouselWidget::begin([
-        'container' => 'div',
-        'assetType' => OwlCarouselWidget::ASSET_TYPE_LOCAL,
-        'jqueryFunction' => '$',// or $
-        'containerOptions' => [
-            'id' => 'container-id',
-            'class' => 'container-class owl-theme',
-            'style'=>'width:100%;padding-top:70px; height:100%;'
-        ],
-        'pluginOptions' => [
-            'autoplay' => true,
-            'autoplayTimeout' => 3000,
-            'items' => 1,
-            'loop' => true,
-            'itemsDesktop' => [1199, 3],
-            'itemsDesktopSmall' => [979, 3]
-        ]
-    ]);
-    ?>
+<div class="row">
+    <div class="col-12">
+        <?php
+        OwlCarouselWidget::begin([
+            'container' => 'div',
+            'assetType' => OwlCarouselWidget::ASSET_TYPE_LOCAL,
+            'jqueryFunction' => '$',// or $
+            'containerOptions' => [
+                'id' => 'container-id',
+                'class' => 'container-class owl-theme',
+                'style' => 'padding-top:15vh'
+            ],
+            'pluginOptions' => [
+                'autoplay' => true,
+                'autoplayTimeout' => 3000,
+                'items' => 1,
+                'loop' => true,
 
-    <?php foreach ($model as $item) : ?>
-        <div class="item-class"><img src="<?= Yii::getAlias('@web') . '/images/' . $item->img ?>" alt="Image" style="width: 100%;height: 100%;max-width: content-box;max-height: content-box"></div>
-    <?php endforeach; ?>
+            ]
+        ]);
+        ?>
 
-    <?php OwlCarouselWidget::end(); ?>
+        <?php foreach ($model as $item) : ?>
+            <div class="item-class"><img src="<?= Yii::getAlias('@web') . '/images/' . $item->img ?>" alt="Image"
+                                         style="width: 100%;height: 100%;">
+            </div>
+        <?php endforeach; ?>
+
+        <?php OwlCarouselWidget::end(); ?>
+    </div>
+
 </div>
-
