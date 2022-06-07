dabf05c6bb7a7460e5029065bb7e18584ddefbff
Qalandar Dev <qalandar@urdu.uz>
2022-06-07 17:29:18 +0500

    +carusel

--- frontend/views/site/foto.php
+++ frontend/views/site/foto.php
@@ -9,28 +9,33 @@ use backend\models\Foto;
 use kv4nt\owlcarousel\OwlCarouselWidget;
 use yii\bootstrap\Html;
 
-OwlCarouselWidget::begin([
-    'container' => 'div',
-    'assetType' => OwlCarouselWidget::ASSET_TYPE_CDN,
-    'jqueryFunction' => '$',// or $
-    'containerOptions' => [
-        'id' => 'container-id',
-        'class' => 'container-class owl-theme'
-    ],
-    'pluginOptions' => [
-        'autoplay' => true,
-        'autoplayTimeout' => 3000,
-        'items' => 3,
-        'loop' => true,
-        'itemsDesktop' => [1199, 3],
-        'itemsDesktopSmall' => [979, 3]
-    ]
-]);
 ?>
-<?php foreach ($model as $item) : ?>
-    <div class="item-class"><img src="<?= Yii::getAlias('@web') . '/images/' . $item->img ?>" alt="Image 2"></div>
-<?php endforeach; ?>
+<div>
+    <?php
+    OwlCarouselWidget::begin([
+        'container' => 'div',
+        'assetType' => OwlCarouselWidget::ASSET_TYPE_LOCAL,
+        'jqueryFunction' => '$',// or $
+        'containerOptions' => [
+            'id' => 'container-id',
+            'class' => 'container-class owl-theme',
+            'style'=>'width:100%;padding-top:70px; height:100%;'
+        ],
+        'pluginOptions' => [
+            'autoplay' => true,
+            'autoplayTimeout' => 3000,
+            'items' => 1,
+            'loop' => true,
+            'itemsDesktop' => [1199, 3],
+            'itemsDesktopSmall' => [979, 3]
+        ]
+    ]);
+    ?>
 
-<?php OwlCarouselWidget::end(); ?>
+    <?php foreach ($model as $item) : ?>
+        <div class="item-class"><img src="<?= Yii::getAlias('@web') . '/images/' . $item->img ?>" alt="Image" style="width: 100%;height: 100%;max-width: content-box;max-height: content-box"></div>
+    <?php endforeach; ?>
 
+    <?php OwlCarouselWidget::end(); ?>
+</div>
 
